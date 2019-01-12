960e451378b83994404bda38bdf5044d3c1e8ea5
Daniel O'Connell <tojad99@gmail.com>
2019-01-12 15:15:33 +0100

    Dict tests

--- src/exercises/dicts.py
+++ src/exercises/dicts.py
@@ -1,5 +1,49 @@
+"""Dictionary exercises.
 
+Fill out the function definition and run `pylama tests/test_dicts.py`.
+"""
 
 
 def word_lengths(words):
-    return {i: len(i) for i in words}
+    """Return a dict containing each word of the given `words` with its length.
+
+    Examples:
+
+         word_lengths(['ala', 'ma', 'kota']) == {'ala': 3, 'ma': 2, 'kota': 4}
+         word_lengths(['longer', 'phrases']) == {'longer': 6, 'phrases': 7}
+    """
+
+
+def reverse_dict(dic):
+    """Reverse the given dict.
+
+    Example:
+
+         reverse_dict({"a": 1, "b": 2, "c": 3}) == {1: a", 2: "b", 3: "c"}
+    """
+    return {v: k for k, v in dic.items()}
+
+
+def remove_keys(dic, keys):
+    """Return a dict with the given keys removed.
+
+    Example:
+
+        remove_keys({"a": 1, "b", 3, "c": 3}, ["a", "c"]) == {"b": 3}
+    """
+
+
+def zip_dict(keys, values):
+    """Make a dict from the given keys and values.
+
+    The first key is the key of the first value. The second key is
+    the key of the second value, etc. Keep matching them into pairs
+    until at least one list has no more items.
+
+    Examples:
+
+        zip_dic([1, 2, 3], ["a", "b", "c]) == {1: "a", 2: "b", 3: "c"}
+
+    Hints:
+    * zip - https://docs.python.org/3.3/library/functions.html#zip
+    """
--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -1,6 +1,8 @@
 import pytest
 
-from exercises.dicts import word_lengths
+from exercises.dicts import (
+    word_lengths, reverse_dict, remove_keys, zip_dict
+)
 
 
 @pytest.mark.parametrize('words, expected', (
@@ -9,3 +11,34 @@ from exercises.dicts import word_lengths
 ))
 def test_word_len(words, expected):
     assert word_lengths(words) == expected
+
+
+@pytest.mark.parametrize('dic, expected', (
+    ({"a": 1, "b": 2, "c": 3}, {1: "a", 2: "b", 3: "c"}),
+    ({}, {}),
+))
+def test_reverse_dict(dic, expected):
+    assert reverse_dict(dic) == expected
+
+
+@pytest.mark.parametrize('dic, keys, expected', (
+    ({"a": 1, "b": 2, "c": 3}, [], {"a": 1, "b": 2, "c": 3}),
+    ({"a": 1, "b": 2, "c": 3}, ["asda", 1, 2, 3], {"a": 1, "b": 2, "c": 3}),
+    ({"a": 1, "b": 2, "c": 3}, ["a"], {"b": 2, "c": 3}),
+    ({"a": 1, "b": 2, "c": 3}, ["a", "b", "c", "d"], {}),
+    ({}, [1, 2, 3], {}),
+))
+def test_remove_keys(dic, keys, expected):
+    assert remove_keys(dic, keys) == expected
+
+
+@pytest.mark.parametrize('keys, vals, expected', (
+    ([1, 2, 3], ["a", "b", "c"], {1: "a", 2: "b", 3: "c"}),
+    ("abc", "cde", {"a": "c", "b": "d", "c": "e"}),
+    ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], ["a", "b", "c"], {1: "a", 2: "b", 3: "c"}),
+    ([1, 2, 3], ["a", "b", "c", 'd', 'e', 'f', 'g'], {1: "a", 2: "b", 3: "c"}),
+    ([1, 2, 3], [], {}),
+    ([], ["a", "b", "c"], {}),
+))
+def test_zip_dict(keys, vals, expected):
+    assert zip_dict(keys, vals) == expected
