43d9b8c1c20913114528b9970cfb65c5cca9a88b
Daniel O'Connell <tojad99@gmail.com>
2019-01-12 14:52:39 +0100

    Flesh out list tests

--- src/exercises/lists.py
+++ src/exercises/lists.py
@@ -1,4 +1,70 @@
+"""List exercises.
+
+Fill out the functions with your implementation, then run `pylama tests/test_lists.py`.
+"""
 
 
 def make_ala_ma_kota():
-    return ['ala', 'ma', 'kota']
+    """Return a list with 3 elements - 'ala', 'ma' and 'kota'. """
+
+
+def custom_range(start, end):
+    """Return a list of numbers from start (inclusive) to end (exclusive).
+
+    Examples:
+
+        > custom_range(1, 5)
+        [1, 2, 3, 4]
+        > custom_range(3, 6)
+        [3, 4, 5]
+    """
+
+
+def only_odds(numbers):
+    """Go through the given `number` and remove only the odd ones.
+
+    e.g.: only_odds([1, 2, 3, 3, 4, 4, 5]) == [1, 3, 5]
+    """
+
+
+def middle_n(numbers, size):
+    """Return the `size` middle items of the given list.
+
+    Assume that size is always odd and that there are always an odd
+    number of numbers (it makes things easier).
+
+    Examples:
+
+        > middle_n([1, 2, 3, 4, 5, 6, 7], 1) == [4]
+        > middle_n([1, 2, 3, 4, 5, 6, 7], 3) == [3, 4, 5]
+        > middle_n([1, 2, 3], 100) == [1, 2, 3]
+
+    Hints:
+     * `len(numbers) / 2`
+     * first work out where the slice should start from
+    """
+
+
+def reverse_list(lista):
+    """Return the given list reversed.
+
+    e.g. reverse_list([1, 2, 3, 4]) == [4, 3, 2, 1]
+    """
+
+
+def is_list(to_check):
+    """Check whether the given thingy is a list.
+
+    This is a helper function to make the next one a bit easier.
+
+    :param to_check: The thing to check
+    :return: whether `to_check` is a list (True or False)
+    """
+    return isinstance(to_check, list)
+
+
+def flatten(lista):
+    """Flatten the given list.
+
+    e.g.: flatten([1, [[2, 3], [4, 5]], [[[[6]]]]]) == [1, 2, 3, 4, 5, 6]
+    """
--- tests/test_lists.py
+++ tests/test_lists.py
@@ -1,5 +1,63 @@
-from exercises.lists import make_ala_ma_kota
+import pytest
+from exercises.lists import (
+    make_ala_ma_kota, custom_range, reverse_list, flatten,
+    only_odds, middle_n
+)
 
 
 def test_make_ala_ma_kota():
     assert make_ala_ma_kota() == ['ala', 'ma', 'kota']
+
+
+@pytest.mark.parametrize('start, end, expected', (
+    (1, 10, [1, 2, 3, 4, 5, 6, 7, 8, 9]),
+    (5, 12, [5, 6, 7, 8, 9, 10, 11]),
+    (1, 2, [1]),
+    (1, 1, []),
+    (5, 2, [])
+))
+def test_custom_range(start, end, expected):
+    assert custom_range(start, end) == expected
+
+
+@pytest.mark .parametrize('lista, expected', (
+    ([1, 2, 3, 4], [1, 3]),
+    ([1, 1, 1, 1], [1, 1, 1, 1]),
+    ([2, 2, 2, 2], []),
+    ([1, 5, 3, 7, 2, 8, 5], [1, 5, 3, 7, 5]),
+    ([], []),
+))
+def test_only_odds(lista, expected):
+    assert only_odds(lista) == expected
+
+
+@pytest.mark .parametrize('numbers, size, expected', (
+    ([1, 2, 3, 4, 5, 6, 7, 8, 9], 1, [5]),
+    ([1, 2, 3, 4, 5, 6, 7, 8, 9], 5, [3, 4, 5, 6, 7]),
+    ([4, 5, 6, 7, 8, 9, 10, 11, 12], 5, [6, 7, 8, 9, 10]),
+    ([5, 6, 7, 8, 9], 10000, [5, 6, 7, 8, 9]),
+    ([], 1, []),
+))
+def test_middle_n(numbers, size, expected):
+    assert middle_n(numbers, size) == expected
+
+
+
+@pytest.mark .parametrize('lista, expected', (
+    ([1, 2, 3, 4], [4, 3, 2, 1]),
+    ([1, [2, 3], 4, 5], [5, 4, [2, 3], 1]),
+    ([], []),
+))
+def test_reverse_list(lista, expected):
+    assert reverse_list(lista) == expected
+
+
+@pytest.mark .parametrize('lista, expected', (
+    ([1, 2, 3, 4], [1, 2, 3, 4]),
+    ([1, [2, 3], 4, 5], [1, 2, 3, 4, 5]),
+    ([1, [2, 3], [4, 5]], [1, 2, 3, 4, 5]),
+    ([[[[1]]], [2, 3], [4, 5]], [1, 2, 3, 4, 5]),
+    ([], []),
+))
+def test_flatten(lista, expected):
+    assert flatten(lista) == expected
